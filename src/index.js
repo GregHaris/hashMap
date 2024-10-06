cccdd98f09adf40ec7f0b7fd6ac8e52e9bbaee20
Grëg Häris <d.gregharis@gmail.com>
2024-10-06 12:07:58 +0100

    Create Hashmap class and hash function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+class HashMap {
+  constructor(size = 50) {
+    this.buckets = new Array(size);
+    this.size = size;
+  }
+  hash(key) {
+    let hashCode = 0;
+    let i = 0;
 
+    const primeNumber = 31;
+    while (i < key.length) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.size;
+      i++;
+    }
+    return hashCode;
+  }
+}
